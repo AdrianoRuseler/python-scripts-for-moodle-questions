2d189b51ec3654c4417ece1c3039c48d8d0a87af
Adriano Ruseler <adrianoruseler@gmail.com>
2023-09-16 13:34:16 -0300

    Update createdescriptionquestions.py

--- QuestionBank/description/createdescriptionquestions.py
+++ QuestionBank/description/createdescriptionquestions.py
@@ -3,18 +3,19 @@ fake = Faker()
 import xml.etree.cElementTree as ET
 
 quiz = ET.Element("quiz")
-quiz.append(ET.Comment(fake.catch_phrase())) # This is a comment
+q=0 # Question number
+quiz.append(ET.Comment("description quiz type number: "+str(q+1))) # This is a comment
 
 # Question Loop
 question = ET.SubElement(quiz, "question", type="description") # Question Type
 
 # Question Name
 name=ET.SubElement(question, "name")
-ET.SubElement(name, "text").text = fake.catch_phrase()
+ET.SubElement(name, "text").text = "description "+str(q+1)+': '+fake.catch_phrase()
 
 # Question Text
 questiontext=ET.SubElement(question, "questiontext", format="html")
-ET.SubElement(questiontext,"text").text = fake.text()
+ET.SubElement(questiontext,"text").text = "description "+str(q+1)+";<br>"+fake.text()
 
 generalfeedback=ET.SubElement(question, "generalfeedback", format="html")
 ET.SubElement(generalfeedback,"text").text = fake.text()
@@ -22,7 +23,7 @@ ET.SubElement(generalfeedback,"text").text = fake.text()
 ET.SubElement(question, "defaultgrade").text = "0"
 ET.SubElement(question, "penalty").text = "0"
 ET.SubElement(question, "hidden").text = "0"
-ET.SubElement(question, "idnumber").text = fake.word()
+ET.SubElement(question, "idnumber").text = "description"+str(q+1)
 
 tree = ET.ElementTree(quiz)
 ET.indent(tree)
