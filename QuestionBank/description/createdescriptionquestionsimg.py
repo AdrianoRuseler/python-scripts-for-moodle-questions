d6cbfe6c1f18ee8c275f8c60ceb30cf00ad2faf4
Adriano Ruseler <adrianoruseler@gmail.com>
2023-09-16 13:33:56 -0300

    Update createdescriptionquestionsimg.py

--- QuestionBank/description/createdescriptionquestionsimg.py
+++ QuestionBank/description/createdescriptionquestionsimg.py
@@ -5,20 +5,19 @@ fake = Faker()
 import xml.etree.cElementTree as ET
 
 quiz = ET.Element("quiz")
-quiz.append(ET.Comment(fake.catch_phrase())) # This is a comment
+q=0 # Question number
+quiz.append(ET.Comment("description quiz type number: "+str(q+1))) # This is a comment
 
 # Question Loop
 question = ET.SubElement(quiz, "question", type="description") # Question Type
 
 # Question Name
 name=ET.SubElement(question, "name")
-ET.SubElement(name, "text").text = fake.catch_phrase()
+ET.SubElement(name, "text").text = "description "+str(q+1)+': '+fake.catch_phrase()
 
 # Question Text
 questiontext=ET.SubElement(question, "questiontext", format="html")
 
-
-#name="839-200x200.jpg" path="/" encoding="base64"
 # Getting image in bytes
 response = requests.get("https://picsum.photos/200") 
 imgname=(response.headers.get("Content-Disposition").split("filename=")[1]).replace('"', '') 
@@ -28,12 +27,18 @@ ET.SubElement(questiontext,"text").text = fake.text()+imginsert
 ET.SubElement(questiontext,"file",name=imgname.strip(),path="/",encoding="base64").text = base64.b64encode(response.content).decode("utf-8")
 
 generalfeedback=ET.SubElement(question, "generalfeedback", format="html")
-ET.SubElement(generalfeedback,"text").text = fake.text()
+# Getting image in bytes
+response = requests.get("https://picsum.photos/200") 
+imgname=(response.headers.get("Content-Disposition").split("filename=")[1]).replace('"', '') 
+imginsert='<p><img class="img-fluid align-top" src="@@PLUGINFILE@@/'+imgname+'" alt="'+fake.catch_phrase()+'" width="200" height="200"></p>'
+
+ET.SubElement(generalfeedback,"text").text = fake.text()+imginsert
+ET.SubElement(generalfeedback,"file",name=imgname.strip(),path="/",encoding="base64").text = base64.b64encode(response.content).decode("utf-8")
 
 ET.SubElement(question, "defaultgrade").text = "0"
 ET.SubElement(question, "penalty").text = "0"
 ET.SubElement(question, "hidden").text = "0"
-ET.SubElement(question, "idnumber").text = fake.word()
+ET.SubElement(question, "idnumber").text = "description"+str(q+1)
 
 tree = ET.ElementTree(quiz)
 ET.indent(tree)
