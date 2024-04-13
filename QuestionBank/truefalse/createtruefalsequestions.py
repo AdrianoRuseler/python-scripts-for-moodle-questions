8cf0ece0672915da58e11ea4888707cd90b550c1
Adriano Ruseler <adrianoruseler@gmail.com>
2024-04-13 13:33:08 -0300

    Update createtruefalsequestions.py

--- QuestionBank/truefalse/createtruefalsequestions.py
+++ QuestionBank/truefalse/createtruefalsequestions.py
@@ -9,13 +9,25 @@ quiz.append(ET.Comment(fake.catch_phrase())) # This is a comment
 # Question Loop
 question = ET.SubElement(quiz, "question", type="truefalse") # Question Type
 
+if fake.pybool():
+  print("True!")
+  qtext=fake.sentence() + ' (True!)'# Question text 
+  faf="0"
+  taf="100"
+else:
+  print("False!")
+  qtext=fake.sentence() + ' (False!)'# Question text
+  faf="100"
+  taf="0"
+  
+
 # Question Name
 name=ET.SubElement(question, "name")
-ET.SubElement(name, "text").text = fake.catch_phrase() # Question name 
+ET.SubElement(name, "text").text = fake.sentence() # Question name 
 
 # Question Text
 questiontext=ET.SubElement(question, "questiontext", format="html")
-ET.SubElement(questiontext,"text").text = fake.sentence() # Question text 
+ET.SubElement(questiontext,"text").text = qtext # Question text 
 
 generalfeedback=ET.SubElement(question, "generalfeedback", format="html")
 ET.SubElement(generalfeedback,"text").text = fake.text() # General feedback 
@@ -25,15 +37,15 @@ ET.SubElement(question, "penalty").text = "1"
 ET.SubElement(question, "hidden").text = "0"
 ET.SubElement(question, "idnumber").text = fake.word()
  
-trueanswer=ET.SubElement(question, "answer", fraction="0", format="moodle_auto_format") 
+trueanswer=ET.SubElement(question, "answer", fraction=taf, format="moodle_auto_format") 
 ET.SubElement(trueanswer,"text").text = "true"
 truefeedback=ET.SubElement(trueanswer,"feedback", format="html")
-ET.SubElement(truefeedback,"text").text = fake.catch_phrase()
+ET.SubElement(truefeedback,"text").text = fake.catch_phrase() + ' (true)!'
 
-falseanswer=ET.SubElement(question, "answer", fraction="100", format="moodle_auto_format") 
+falseanswer=ET.SubElement(question, "answer", fraction=faf, format="moodle_auto_format") 
 ET.SubElement(falseanswer,"text").text = "false"
 falsefeedback=ET.SubElement(falseanswer,"feedback", format="html")
-ET.SubElement(falsefeedback,"text").text = fake.catch_phrase()
+ET.SubElement(falsefeedback,"text").text = fake.catch_phrase() + ' (false)!'
     
 tree = ET.ElementTree(quiz)
 ET.indent(tree)
